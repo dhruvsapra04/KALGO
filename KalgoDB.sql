0e0e21ab189509759e08f264281f180d1f9e0441
dhruvsapra04 <65138252+dhruvsapra04@users.noreply.github.com>
2025-06-12 16:44:33 -0400

    Update KalgoDB.sql

--- KalgoDB.sql
+++ KalgoDB.sql
@@ -1,9 +1,19 @@
 CREATE DATABASE IF NOT EXISTS stock_db;
-
 USE stock_db;
 
 CREATE TABLE IF NOT EXISTS live_stock_prices (
     symbol VARCHAR(10),
     price DECIMAL(10,2),
     timestamp DATETIME
+);
+
+CREATE TABLE IF NOT EXISTS stock_prices (
+    symbol VARCHAR(10),
+    price DECIMAL(10,2),
+    timestamp DATETIME
+);
+
+CREATE TABLE IF NOT EXISTS sma_20d (
+    symbol VARCHAR(10) PRIMARY KEY,
+    sma DECIMAL(10,4)
 );
